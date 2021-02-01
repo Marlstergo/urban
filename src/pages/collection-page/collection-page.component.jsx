2e4337b037c2a0dfb86a0fe602ae3837cfa643d3
Marlstergo <abdulmaliqshola@gmail.com>
2021-02-01 23:21:46 +0100

    uncompleted colectioin page compnent statetoprops

---
+++ src/pages/collection-page/collection-page.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './collection-page.styles.scss'
+
+const CollectionPage = ({match}) =>(
+    <div className='category'> 
+        <h2>COLLECTION PAGE</h2>
+    </div>
+)
+
+
+export default CollectionPage
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './shop.style.scss';
 import { Route } from 'react-router-dom'
 // import SHOP_DATA from './shop.data';
-
+import CollectionPage from '../collection-page/collection-page.component'
 import CollectionOverview from '../../components/collection-overview/collection-overview.component'
 
 
 const ShopPage = ({match}) => (       
     <div className='shop-page'>
         <Route exact path={`${match.path}`} component={CollectionOverview}/>
-            
+        <Route exact path={`${match.path}/:collectionId` } component={CollectionPage} />
     </div>
 )
 
