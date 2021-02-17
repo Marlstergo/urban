71250197634fb2698bef427824f0dd768c53b79e
Marlstergo <abdulmaliqshola@gmail.com>
2021-02-17 19:20:35 +0100

    debugging links

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -6,7 +6,7 @@ import {withRouter} from 'react-router-dom'
 
 const CollectionPreview = ({title, items, routeName, history, match }) =>(
     <div className='collection-preview'>
-        <h1 className='title' onClick={() =>(history.push(`${match.url}${routeName}`))}>{title.toUpperCase()}</h1>
+        <h1 className='title' onClick={() =>(history.push(`${match.url}/${routeName}`))}>{title.toUpperCase()}</h1>
         <div className='preview'>
             {items
                 .filter((item , idx) => idx < 4)
