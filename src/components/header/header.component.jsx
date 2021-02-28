2f6644bbbd92be8f39d7898f6996cfa3143d1128
Marlstergo <abdulmaliqshola@gmail.com>
2021-02-28 22:31:03 +0100

    commit

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crown.svg'
+
 import {auth} from '../../firebase/firebase.util'
 import {connect} from 'react-redux'
 import CartIcon from '../cart-icon/cart-icon.component'
